2ff36655abc074795c60a3b21cd5364a54dcffbb
huzee-15 <_huzaifeee53*>
2025-12-05 09:26:18 +0500

    Add project files.

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Models;
+
+namespace technova_ecommerce.Controllers
+{
+    public class AuthController : Controller
+    {
+        private  DatabaseContext _db; 
+        public AuthController(DatabaseContext db)   
+        {
+            _db = db;
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
