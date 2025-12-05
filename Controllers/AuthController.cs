9862c9cb4503d3e909920d88a18b3a52517ec96f
huzee-15 <_huzaifeee53*>
2025-12-05 09:50:15 +0500

    today

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -1,12 +1,14 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Models;
+using technova_ecommerce.Models.Entities;
 
 namespace technova_ecommerce.Controllers
 {
     public class AuthController : Controller
     {
-        private  DatabaseContext _db; 
-        public AuthController(DatabaseContext db)   
+        private DatabaseContext _db;
+
+        public AuthController(DatabaseContext db)
         {
             _db = db;
         }
@@ -20,5 +22,27 @@ namespace technova_ecommerce.Controllers
         {
             return View();
         }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(User user)
+        {
+            if (ModelState.IsValid)
+            {
+                if (_db.Users.Any(u => u.UserName == user.UserName))
+                {
+                    ViewBag.Error = "Username already exists. Please try another username.";
+                    return View(user);
+                }
+
+                
+                user.HashedPassword = BCrypt.Net.BCrypt.HashPassword(user.HashedPassword);  
+                _db.Users.Add(user);
+                await _db.SaveChangesAsync();
+                return RedirectToAction("Login", "Auth");
+            }
+
+            return View(user);
+        }
     }
+
 }
